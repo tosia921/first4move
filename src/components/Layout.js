277d8ef0fd81ed43a9d6760383331159bbb8d54c
Tomasz Posiadala <tomasz.posiadala@jaywing.com>
2022-10-10 22:52:59 +0100

    test

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,11 @@ const Layout = ({ children }) => {
 	return (
 		<div className="min-h-screen flex flex-col">
 			<Header />
-			<main className="grow px-4 md:px-16 lg:px-[6rem] xl:px-0 xl:w-[1024px] xl:mx-auto xxl:w-[1240px] xxxl:w-[1440px]">{children}</main>
+			<main
+			// className="px-4 md:px-16 lg:px-[6rem] xl:px-0 xl:w-[1024px] xl:mx-auto xxl:w-[1240px] xxxl:w-[1440px]"
+			>
+				{children}
+			</main>
 			<Footer />
 		</div>
 	);
