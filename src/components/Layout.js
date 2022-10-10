40507f6b4e4230b8042bbe5bcdf1559b14cd1cbe
Tomasz Posiadala <tomasz.posiadala@jaywing.com>
2022-10-10 22:56:54 +0100

    page padding

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,11 +6,7 @@ const Layout = ({ children }) => {
 	return (
 		<div className="min-h-screen flex flex-col">
 			<Header />
-			<main
-			// className="px-4 md:px-16 lg:px-[6rem] xl:px-0 xl:w-[1024px] xl:mx-auto xxl:w-[1240px] xxxl:w-[1440px]"
-			>
-				{children}
-			</main>
+			<main className="page-padding">{children}</main>
 			<Footer />
 		</div>
 	);
