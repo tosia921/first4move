af3c785138f67e1769867dac3dbd7e1d7367c830
Tomasz Posiadala <tomasz.posiadala@jaywing.com>
2022-10-20 16:36:44 +0100

    chanegs to form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,51 +8,55 @@ const ContactForm = () => {
 				<div>
 					<div className="flex flex-wrap -mx-3 mb-6">
 						<div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-							<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+							<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="FirstName">
 								First Name
 							</label>
 							<input
 								className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
 								id="grid-first-name"
 								type="text"
-								placeholder="Jane"
+								placeholder="Enter your Name..."
+								name="FirstName"
 							/>
 							<p className="text-red-500 text-xs italic">Please fill out this field.</p>
 						</div>
 						<div className="w-full md:w-1/2 px-3">
-							<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+							<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="LastName">
 								Last Name
 							</label>
 							<input
 								className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
 								id="grid-last-name"
 								type="text"
-								placeholder="Doe"
+								placeholder="Enter your Last Name..."
+								name="LastName"
 							/>
 						</div>
 					</div>
 					<div className="flex flex-wrap -mx-3 mb-6">
 						<div className="w-full px-3">
-							<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+							<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="Email">
 								E-mail
 							</label>
 							<input
 								className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
 								id="email"
 								type="email"
+								name="Email"
 							/>
 							<p className="text-gray-600 text-xs italic">Some tips - as long as needed</p>
 						</div>
 					</div>
 					<div className="flex flex-wrap -mx-3 mb-6">
 						<div className="w-full px-3">
-							<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+							<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="Phone">
 								Phone Number
 							</label>
 							<input
 								className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
 								id="phone"
 								type="text"
+								name="Phone"
 							/>
 							<p className="text-gray-600 text-xs italic">Some tips - as long as needed</p>
 						</div>
@@ -60,12 +64,13 @@ const ContactForm = () => {
 				</div>
 				<div className="flex w-full flex-wrap mb-6 md:ml-5">
 					<div className="w-full ">
-						<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+						<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="Message">
 							Message
 						</label>
 						<textarea
 							className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-72 md:h-full resize-none"
 							id="message"
+							name="Message"
 						></textarea>
 					</div>
 				</div>
@@ -73,6 +78,7 @@ const ContactForm = () => {
 			<input type="hidden" name="_next" value="https://first4move.netlify.app/email-success-page" />
 			<input type="hidden" name="_subject" value="New email form submission from First4Move.co.uk"></input>
 			<input type="text" name="_honey" style={{ display: 'none' }}></input>
+			<input type="hidden" name="_template" value="table"></input>
 			<div className="mt-5 md:mt-0">
 				<button
 					type="submit"
