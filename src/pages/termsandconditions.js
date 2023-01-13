8e0bbe70b3f0a287df624d72a1583fc5be8b37ab
Tomasz Posiadala <tomasz.posiadala@jaywing.com>
2023-01-13 21:53:03 +0000

    terms update

--- src/pages/termsandconditions.js
+++ src/pages/termsandconditions.js
@@ -9,20 +9,15 @@ const termsandconditions = () => {
 					<h1 className="font-Baloo text-3xl mb-4">Website usage terms and conditions</h1>
 					<p className="mb-2">
 						Welcome to our website. If you continue to browse and use this website, you are agreeing to comply with and be bound by the following terms and conditions of use, which
-						together with our privacy policy govern [business name]’s relationship with you in relation to this website.
+						together with our privacy policy govern 123RenovatorEdinburgh’s relationship with you in relation to this website.
 					</p>
 					<p className="mb-2">
-						If you disagree with any part of these terms and conditions, please do not use our website. The term ‘[business name]’ or ‘us’ or ‘we’ refers to the owner of the website whose
-						registered office is [address].
+						If you disagree with any part of these terms and conditions, please do not use our website. The term ‘123RenovatorEdinburgh’ or ‘us’ or ‘we’ refers to the owner of the website whose
+						registered office is at Edinburgh, EH1 24W.
 					</p>
-					<p className="mb-2">Our company registration number is [company registration number and place of registration]. The term ‘you’ refers to the user or viewer of our website.</p>
 					<p className="mb-2">The use of this website is subject to the following terms of use:</p>
 					<ul className="list-disc">
 						<li className="my-2 ml-4 ">The content of the pages of this website is for your general information and use only. It is subject to change without notice.</li>
-						<li className="my-2 ml-4">
-							This website uses cookies to monitor browsing preferences. If you do allow cookies to be used, the following personal information may be stored by us for use by third
-							parties: [insert list of information].
-						</li>
 						<li className="my-2 ml-4">
 							Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness or suitability of the information and materials
 							found or offered on this website for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors and we expressly exclude
